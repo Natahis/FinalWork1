70dda4000a677c69c6b4a2d4f5789a73168803ba
Natahis <natahis@bk.ru>
2022-09-19 12:30:57 +0300

    method

--- Task/Program.cs
+++ Task/Program.cs
@@ -10,3 +10,37 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] array1 = { "Hello", "2", "world", ":-)" };
+string[] array2 = { "1234", "1567", "-2", "computer science" };
+string[] array3 = { "Russia", "Denmark", "Kazan" };
+
+string[] FoundLengtString(string[] array, int size)
+{
+    string[] result = new string[array.Length];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= size)
+        {
+            result[j] = array[i];
+            j++;
+        }
+    }
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+    Console.WriteLine();
+}
+
+string[] arr1 = FoundLengtString(array1, 3);
+PrintArray(arr1);
